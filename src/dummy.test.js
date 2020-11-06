ca6c0b37bc68b327070858050ee3ce32956e7966
Daniel Fornells <daniel.garcia.fornells@gmail.com>
2020-11-06 09:32:39 +0100

    Add a dynamic table test example

--- src/dummy.test.js
+++ src/dummy.test.js
@@ -1,7 +1,23 @@
 import dummy from './dummy'
 
-describe('A Dummy test file', () => {
-  test('Checks dummy function returns "Hello"', () => {
-    expect(dummy()).toBe('Hello')
+describe('Dummy function', () => {
+  test('Exists', () => {
+    expect(dummy).toBeTruthy()
+    expect(typeof dummy).toBe('function')
+  })
+})
+
+describe.each`
+    input       | expectedResult
+    ${1}        | ${'1'}
+    ${2}        | ${'2'}
+  `('Given $input, expecting $expectedResult', ({ input, expectedResult }) => {
+
+  test(`Input returns the expected result (${input} -> ${expectedResult})`, () => {
+    expect(input.toString()).toBe(expectedResult)
+  })
+
+  test(`Output is a string`, () => {
+    expect(typeof expectedResult).toBe('string')
   })
 })
